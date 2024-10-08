db8e1939ca801aa3629dbf0735497da66e0384df
andrewaminazm <andrewamin878@gmail.com>
2024-10-08 15:51:35 +0300

    katalonpro

---
+++ Test Suites/calculator.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>simple test for run calculator</description>
+   <name>calculator</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cb42b6ae-4cd3-4827-83e8-a066b5a7177b</testSuiteGuid>
+   <testCaseLink>
+      <guid>a62882f8-9c42-40f1-b57c-f447ce1eca20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
